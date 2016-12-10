88666e6c722ab8286ed2bcc763a8cce71cf967eb
Amelia Brown <amelia.brown389@gmail.com>
2016-12-10 12:27:32 -0800

    (feature): rregulation reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,1 +1,16 @@
 import {combineReducers} from 'redux';
+
+import regulations from 'regulation.json';
+
+const regulation = (state={}, action) => {
+  switch (action.type) {
+    case: 'GET_REGULATION':
+      return action.payload,
+    default:
+      return state
+  }
+};
+
+export default combineReducers(
+  regulation,
+);
