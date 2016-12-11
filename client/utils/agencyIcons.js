2c923915ee43c9b6cbccb16c480cad9260acc460
Amelia Brown <amelia.brown389@gmail.com>
2016-12-10 17:56:52 -0800

    (feature): filll out dialog

---
+++ client/utils/agencyIcons.js
@@ -0,0 +1,29 @@
+export default () => ({
+  'Aerospace and Transportation': 'airplanemode_active',
+  'Agriculture, Environment, and Public Lands': '',
+  'Banking and Financial': '',
+  'Commerce and International': '',
+  'Defense, Law Enforcement, and Security': '',
+  'Education, Labor, Presidential, and Government Services': '',
+  'Energy, Natural Resources, and Utilities': '',
+  'Food Safety, Health, and Pharmaceutical': '',
+  'Housing, Development, and Real Estate': '',
+  'Technology and Telecommunications': '',
+})
+
+
+/*
+ *
+ *AD (Aerospace and Transportation)
+AEP (Agriculture, Environment, and Public Lands)
+BFS (Banking and Financial)
+CT (Commerce and International)
+LES (Defense, Law Enforcement, and Security)
+EELS (Education, Labor, Presidential, and Government Services)
+EUMM (Energy, Natural Resources, and Utilities)
+HCFP (Food Safety, Health, and Pharmaceutical)
+PRE (Housing, Development, and Real Estate)
+ITT (Technology and Telecommunications)
+query
+ *
+ * */
