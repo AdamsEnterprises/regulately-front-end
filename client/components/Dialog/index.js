53d6a78211e11dd0d630e6825339ff01fd6d4584
Amelia Brown <amelia.brown389@gmail.com>
2016-12-10 16:12:08 -0800

    (feature): configure modal, attempt widget

--- client/components/Dialog/index.js
+++ client/components/Dialog/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-
+import React, {component} from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import {connect} from 'react-redux';
 
-const Dialog = ({regulation}) => (
-  <div>
-    {regulation.title}
-  </div>
+const Modal = ({regulation}) => (
+  <Dialog
+    title="Dialog"
+    open={true}
+    modal={true}>
+    <div style={{overflow:"show"}}>
+      <div>{regulation.title}</div>
+      <div>{regulation.abstract}</div>
+    </div>
+  </Dialog>
 )
-
 const mapStateToProps = ({regulation}) => ({
   regulation
 })
 
-export default connect(mapStateToProps)(Dialog);
+export default connect(mapStateToProps)(Modal);
